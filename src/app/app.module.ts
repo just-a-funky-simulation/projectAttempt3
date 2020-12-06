b6b3c3ac4a18f4ac7b4dc92c22bfe03af85a1c59
marilena.sayfullina@gmail.com <D6robotdays>
2020-12-06 19:55:47 +1100

    all mat design works, login started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatSliderModule } from '@angular/material/slider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -8,6 +11,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './login/login.component';
 import { FilterComponent } from './filter/filter.component';
+import {MatRadioModule} from '@angular/material/radio';
 
 @NgModule({
   declarations: [
@@ -20,6 +24,9 @@ import { FilterComponent } from './filter/filter.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSliderModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatRadioModule,
 
   ],
   providers: [],
