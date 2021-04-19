246b269d8d18fed19b106ab271adaa1d6c3a8b81
marilena.sayfullina@gmail.com <D6robotdays>
2021-04-19 22:04:12 +1000

    tiles working with database

--- src/app/expanded-view/expanded-view.component.ts
+++ src/app/expanded-view/expanded-view.component.ts
@@ -17,14 +17,14 @@ export interface Tile {
 export class ExpandedViewComponent implements OnInit {
 
   tiles: Tile[] = [
-  	{text: 'Genre', cols: 1, rows: 1, color: 'lightpink'},
+  	{text: movies[400].genre, cols: 1, rows: 1, color: 'lightpink'},
 
-       {text: 'Title', cols: 2, rows: 1, color: 'lightgreen'},
+       {text: movies[400].title, cols: 2, rows: 1, color: 'lightgreen'},
 
-      {text: 'Ratinng', cols: 1, rows: 1, color: '#DDBDF1'},
+      {text: 'Rating', cols: 1, rows: 1, color: '#DDBDF1'},
 
       {text: 'Poster', cols: 2, rows: 7, color: 'lightblue'},
-      {text: 'Description', cols: 2, rows: 7, color: 'yellow'},
+      {text: movies[400].description, cols: 2, rows: 7, color: 'yellow'},
 
       {text: 'Seen', cols: 1, rows: 1, color: 'green'},
       {text: 'Watch', cols: 2, rows: 1, color: 'indigo'},
