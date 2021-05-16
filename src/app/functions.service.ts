5b85bc33c72ac53bb802453907416f874fbd311e
marilena.sayfullina@gmail.com <D6robotdays>
2021-05-16 12:40:09 +1000

    watched movie works, css doing

--- src/app/functions.service.ts
+++ src/app/functions.service.ts
@@ -102,9 +102,5 @@ export class FunctionsService {
   }
 
 }
-/// FUNCTIONS TO TODO --- if watched add to watched list, Watched Button add to Watched List
-/// Filter movies by selected filters
-/// Create new array from filtered movies
-/// add images and info to tiles
-/// on new filter click clear array
+
 
--- src/app/watched-movie/watched-movie.component.ts
+++ src/app/watched-movie/watched-movie.component.ts
@@ -29,7 +29,7 @@ row = 421;
     let watchedMovies = movies.filter(iterator => watchedIds.includes(iterator.id) );
 
     this.tiles = watchedMovies.map(iterator => {
-      return {text: iterator.title, cols: 2, rows: 1, color: iterator.descriptioncolour}
+      return {text: iterator.title, cols: 2, rows: 1, color: iterator.descriptioncolour || 'pink'}
     });
 
 
